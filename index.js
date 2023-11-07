596d3b505c3ad60a33d0fbb4e4ace40fd3166d25
poulamib1602 <poulamib.albiorix@gmail.com>
2023-11-07 11:11:02 +0530

    part-4 review changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
-const postRoute = require('./routes/post');
+const postRouter = require('./routes/post');
 const commentsRouter = require('./routes/comments');
 
 
@@ -25,14 +25,14 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use('/api/users',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/posts',postRoute);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
 app.use('/api/comments', commentsRouter);
 
 
 
 app.listen(port, () => {
     logger.info(`Server is running on port ${port}`);
-    
+
 });
--- response.js
+++ response.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 module.exports = {
-    success : (res,data) =>{
-        res.status(200).json({success:true, data});
+    success: (res, data) => {
+        res.status(200).json({ success: true, data });
     },
 
     error: (res, message, statusCode = 500) => {
         res.status(statusCode).json({ success: false, error: message });
-      },
+    },
 };
